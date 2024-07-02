b7e790e7c112fa79249f9b375a5d0237e3aeaaa9
Tannirumamatha <tannirumamatha5@gmail.com>
2024-07-02 18:26:51 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.String;
+class String1
+{
+	public static void main(String[] args)
+	{
+	Scanner s=new Scanner(System.in);
+	String s1[]=new String[5];
+	for(int i=0;i<5;i++)
+	{
+	s1[i]=s.next();
+	}
+	for(int i=0;i<5;i++)
+	{
+	System.out.println(s1[i]);
+	}
+	}
+}
