032b770bb0936a2c4beef9689534bc4c2dcb6586
Tannirumamatha <tannirumamatha5@gmail.com>
2024-07-02 16:27:18 +0530

    Add files via upload

---
+++ InversionPair.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class InversionPair
+{
+public static void main(String[] args)
+{
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+	int a[]=new int[n];
+	for(int i=0;i<n;i++)
+	{
+		a[i]=s.nextInt();
+	}
+	for(int i=0;i<n-1;i++)
+	{
+	if(a[i]<a[i+1])
+	int c;
+	c==o;
+	c++;
+		System.out.print(c);
+	}
+	System.out.print(a[n-1]);
+}
+}
