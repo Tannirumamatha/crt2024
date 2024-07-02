ef181092b6b390482ad590ae46f85f3858775125
Tannirumamatha <tannirumamatha5@gmail.com>
2024-07-02 16:27:55 +0530

    Add files via upload

---
+++ Lower.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Lower
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String str="Java IS simple LanguAGE";
+System.out.println(str.toLowerCase());
+System.out.println(str.toUpperCase());
+}
+}
+
+
