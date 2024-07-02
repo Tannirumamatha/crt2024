c29e836d77342e401e96dff08c70678703eb4f4e
Tannirumamatha <tannirumamatha5@gmail.com>
2024-07-02 18:31:57 +0530

    Add files via upload

---
+++ Test13.java
@@ -0,0 +1,19 @@
+import java.lang.String;
+import java.util.*;
+class Test13
+{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        String str="Java is simple language";
+	System.out.println(str.contains("simple"));
+	System.out.println(str.contains("python"));
+	System.out.println(str.contains("lan"));
+	System.out.println(str.startsWith("J"));
+	System.out.println(str.startsWith("A"));
+	System.out.println(str.endsWith("age"));
+    }
+}
+
+
+
