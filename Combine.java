5ed9490f77c86d57ace6ab6aaa1a863d2cc09c07
Tannirumamatha <tannirumamatha5@gmail.com>
2024-07-03 14:23:27 +0530

    Add files via upload

---
+++ Combine.java
@@ -0,0 +1,14 @@
+import java.lang.String;
+import java.util.*;
+class Combine
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+       	String str1="234";	
+	String str2="563";
+	System.out.println(Integer.parseInt(str1)+Integer.parseInt(str2));
+    }
+}
+
+
