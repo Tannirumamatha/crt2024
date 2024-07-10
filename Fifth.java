ca94995645b66dc1978ec4bb9dc0353849732c50
Tannirumamatha <tannirumamatha5@gmail.com>
2024-07-10 11:56:37 +0530

    Add files via upload

---
+++ Fifth.java
@@ -0,0 +1,19 @@
+package pack2;
+import pack1.First1;
+class Fifth
+{
+	void show5()
+	{
+		First1 f1=new First1();
+		//System.out.println("a="+f1.a);
+		//System.out.println("b="+f1.b);
+		//System.out.println("c="+f1.c);
+		System.out.println("d="+f1.d);
+	}
+	public static void main(String[] args)
+	{
+	    Fifth f=new Fifth();
+	    f.show5();
+	}
+}
+		
