76bcadf1bc056797c790830bd253d5f02d1657e2
Tannirumamatha <tannirumamatha5@gmail.com>
2024-07-10 11:55:55 +0530

    Add files via upload

---
+++ Fourth.java
@@ -0,0 +1,18 @@
+package pack2;
+import pack1.First1;
+class Fourth extends First1
+{
+	void show4()
+	{
+		//System.out.println("a="+a);
+		//System.out.println("b="+b);
+		System.out.println("c="+c);
+		System.out.println("d="+d);
+	}
+	public static void main(String[] args)
+	{
+	    Fourth f1=new Fourth();
+	    f1.show4();
+	}
+}
+		
