006794207c9b8d497f25c081fede960c99071fee
Tannirumamatha <tannirumamatha5@gmail.com>
2024-07-10 12:02:56 +0530

    Add files via upload

---
+++ Fvar.java
@@ -0,0 +1,13 @@
+
+class Fvar
+{
+	final int a=111;
+	public static void main(String[] args)
+	{
+		Fvar f1=new Fvar();
+		System.out.println(f1.a);
+		//f1.a=356;
+		System.out.println(f1.a);
+	}
+}
+		
