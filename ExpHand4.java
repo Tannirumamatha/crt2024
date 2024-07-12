aaf58908a71e86437f4a335096517d81dc8ed936
Tannirumamatha <tannirumamatha5@gmail.com>
2024-07-12 11:05:40 +0530

    Add files via upload

---
+++ ExpHand4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class ExpHand4
+{
+public static void main(String args[])
+{
+try
+{
+System.out.println("micky");
+System.out.println(10/0);
+System.out.println("lokesh");
+}
+catch(ArithmeticException e)
+{
+System.out.println(10/2);
+}
+}
+}
